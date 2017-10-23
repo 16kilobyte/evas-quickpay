fc80eccd566d2d1d2bde34a582a4a38f7a1c6876
D.R <dretnan@logicaladdress.com>
2017-10-23 15:27:35 +0100

    Imo Build

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,7 +1,7 @@
-export const BASE = 'http://evas.stack.ng/api/mobile/'
+export const BASE = 'http://imo.evas.ng/api/mobile/'
 export const LOGIN = 'login'
 export const GET_CONFIGURATIONS = 'get_bundle2'
 export const QUICK_PAYMENT = 'quickpayment'
-export const PAYMENT_GATEWAY = 'https://hive-katorbryan.c9users.io/api/v1/'
+export const PAYMENT_GATEWAY = 'https://pie.ng/api/v1/'
 export const PG_CHARGE_WALLET = PAYMENT_GATEWAY.concat('charge')
 export const PG_VERIFY_WALLET = PAYMENT_GATEWAY.concat('charge/verify')
