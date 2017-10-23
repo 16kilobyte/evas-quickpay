d373ca89e00e512d513b452b2cda9bc45ccde877
D.R <dretnan@logicaladdress.com>
2017-10-23 15:33:59 +0100

    Edo build

--- src/config/pie.js
+++ src/config/pie.js
@@ -2,4 +2,4 @@
 export const PUBLIC_KEY = "pklv_84DNylgseQZarpNj"
 export const CUSTOMER = "payments@niger.evas.ng"
 export const INSURANCE_PUBKEY = "pklv_04ZoleQVcy9wHLCt"
-export const DEV_WALLET = "wal_evasDevAtNiger"
+export const DEV_WALLET = "wal_evasDevAtEdo"
